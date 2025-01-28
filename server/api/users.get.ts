024cd951985e3578f145a1a330e6dd7a60fddb58
Killian <killian.boularand@outlook.fr>
2025-01-28 22:28:48 +0100

    :rocket: cors origin header allowed

--- server/api/users.get.ts
+++ server/api/users.get.ts
@@ -1,6 +1,12 @@
 import prisma from "~/prisma";
+import { defineEventHandler } from "h3";
 
 export default defineEventHandler(async (event) => {
+
+  setResponseHeader(event, 'Access-Control-Allow-Origin', '*');
+  setResponseHeader(event, 'Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  setResponseHeader(event, 'Access-Control-Allow-Headers', 'Content-Type');
+  
   const users = await prisma.user.findMany();
   return {
     users,
